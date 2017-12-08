c43abf08ef529adaf0ee330c0ea64c0428d6c25c
Joseph Dalmasso <me@jodalmasso.com>
2017-12-08 15:25:15 -0500

    Yes! I can now pick random movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ParamsPicker from './components/ParamsPicker';
 import './flex.css';
+import './App.css';
 
 class App extends Component {
   render() {
--- src/components/ParamsPicker/ParamsPicker.js
+++ src/components/ParamsPicker/ParamsPicker.js
@@ -6,10 +6,18 @@ class ParamsPicker extends Component {
     super(props);
 
     this.state = {
-      genres: []
+      availableGenres: [],
+      randomMovies: [],
+      genre: '',
+      ratings: '',
+      year: '',
+      selectedMovie: 0,
+      randomPage: 0,
+      yourMovie: undefined
     };
   }
 
+  //makes api call to TMDB to get list of genres available
   componentDidMount() {
     axios
       .get(
@@ -18,17 +26,82 @@ class ParamsPicker extends Component {
       .then(response => {
         let genres = response.data.genres;
         this.setState({
-          genres: genres
+          availableGenres: genres
         });
-        console.log(this.state.genres);
       })
       .catch(function(error) {
         console.log(error);
       });
   }
 
+  //Set the state with the genre, rating, and year selected
   movieParams = e => {
-    console.log(e.target.value);
+    if (e.target.id === 'genre_dropdown') {
+      this.setState({
+        genre: e.target.value
+      });
+    } else if (e.target.id === 'rating_dropdown') {
+      this.setState({
+        ratings: e.target.value
+      });
+    } else {
+      this.setState({
+        year: e.target.value
+      });
+    }
+  };
+
+  //uses the state to make api call to TMDB and get list of movies meeting criteria
+  submitParams = () => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/discover/movie?api_key=754007c908587eeaa8a0798e4a168614&include_adult=false&include_video=false&page=1&primary_release_year=${
+          this.state.year
+        }&vote_average.gte=${this.state.ratings}&with_genres=${
+          this.state.genre
+        }`
+      )
+      .then(response => {
+        let totalPages = response.data.total_pages;
+        let randomPage;
+        if (totalPages <= 1) {
+          randomPage = 1;
+        } else {
+          randomPage = Math.floor(Math.random() * totalPages + 1);
+        }
+        this.setState({
+          randomPage: randomPage
+        });
+        return axios
+          .get(
+            `https://api.themoviedb.org/3/discover/movie?api_key=754007c908587eeaa8a0798e4a168614&include_adult=false&include_video=false&page=1&primary_release_year=${
+              this.state.year
+            }&vote_average.gte=${this.state.ratings}&with_genres=${
+              this.state.genre
+            }&page=${this.state.randomPage}`
+          )
+          .then(response => {
+            this.setState({
+              randomMovies: response.data.results
+            });
+            this.pickRandomMovie();
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+
+  //selects the random movie to be displayed from the list of movies with matching criteria
+  pickRandomMovie = () => {
+    let randomMovies = this.state.randomMovies;
+    let moviePick = Math.floor(Math.random() * randomMovies.length);
+    this.setState({
+      yourMovie: randomMovies[moviePick]
+    });
   };
 
   render() {
@@ -37,16 +110,20 @@ class ParamsPicker extends Component {
     for (let i = year; i >= 1895; i--) {
       yearArray.push(i);
     }
+
     return (
       <div className="text-align">
         <h1>Pick Random Movie Parameters</h1>
         <div className="flex justify-around">
-          <div className="genre_dropdown">
+          <div>
             <h3>Genre</h3>
-            <select onChange={this.movieParams}>
-              {this.state.genres.map(genre => {
+            <select id="genre_dropdown" onChange={this.movieParams}>
+              <option value="" disabled selected>
+                Select genre...
+              </option>
+              {this.state.availableGenres.map(genre => {
                 return (
-                  <option key={genre.id} value={genre.name}>
+                  <option key={genre.id} value={genre.id}>
                     {genre.name}
                   </option>
                 );
@@ -54,10 +131,13 @@ class ParamsPicker extends Component {
             </select>
           </div>
 
-          <div className="rating_dropdown">
-            <h3>Rating Range</h3>
-            <select onChange={this.movieParams}>
-              {[10, 9, 8, 7, 6, 5, 4, 3, 2, 1].map(num => {
+          <div>
+            <h3>Average Ratings</h3>
+            <select id="rating_dropdown" onChange={this.movieParams}>
+              <option value="" disabled selected>
+                Select rating...
+              </option>
+              {[9, 8, 7, 6, 5, 4, 3, 2, 1].map(num => {
                 return (
                   <option key={num} value={num}>
                     {num}
@@ -67,9 +147,12 @@ class ParamsPicker extends Component {
             </select>
           </div>
 
-          <div className="year_dropdown">
-            <h3>Year Range</h3>
-            <select onChange={this.movieParams}>
+          <div>
+            <h3>Release Year</h3>
+            <select id="year_dropdown" onChange={this.movieParams}>
+              <option value="" disabled selected>
+                Select year...
+              </option>
               {yearArray.map(year => {
                 return (
                   <option key={year} value={year}>
@@ -80,11 +163,22 @@ class ParamsPicker extends Component {
             </select>
           </div>
         </div>
+        <input onClick={this.submitParams} type="submit" value="Submit" />
+
+        {this.state.yourMovie ? (
+          <div key={this.state.yourMovie.id}>
+            <h3>{this.state.yourMovie.title}</h3>
+            <img
+              src={`http://image.tmdb.org/t/p/w500/${
+                this.state.yourMovie.poster_path
+              }`}
+              alt="movie_image"
+            />
+          </div>
+        ) : null}
       </div>
     );
   }
 }
 
 export default ParamsPicker;
-
-//'https://api.themoviedb.org/3/discover/movie?api_key=754007c908587eeaa8a0798e4a168614&language=en-US&with_genres=35' add this at the end maybe: &language=en-US
