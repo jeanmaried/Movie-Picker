8487842ac191dfeaac2320408c63b6af056e5c3d
Joseph Dalmasso <me@jodalmasso.com>
2017-12-08 20:48:52 -0500

    Added MUI Theme provider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import ParamsPicker from './components/ParamsPicker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './flex.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ParamsPicker />
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <ParamsPicker />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
